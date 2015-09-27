1632ea3bdce0c219fca7c0e3e1aab27389ac6c40
Felipe Daragon <felipe@206labs.com>
2015-09-27 20:20:20 -0300

    Add compatibility with several additional Lua VMs

---
+++ lua/latclient/luavmjs.lua
@@ -0,0 +1,28 @@
+-- This file is part of the Lua@Client project
+-- Copyright (c) 2015 Etiene Dalcol
+-- License: MIT
+
+local M = {
+	js_served = false,
+	modules_served = {}
+}
+
+local common = require "latclient.common"
+
+function M.get_header(s)
+	if M.js_served == false then
+		M.js_served = true
+		local header = [[
+		<script src="./pub/luavmjs/lua.vm.js"></script>
+		]]
+		s = header..s
+	end
+	return s
+end
+
+function M.get_client_js(s)
+	s = '<script type="text/lua">'..s..'</script>'
+	return M.get_header(s)
+end
+
+return M
